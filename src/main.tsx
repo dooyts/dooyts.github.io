27c5fb0cf9cc60d6cd2807b6de72ff8b6af04de8
yeelunz <asas159230@gmail.com>
2025-06-07 22:28:04 +0800

    feat: Finalize Vite config for GitHub Pages root deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../App'
 import '../index.css'
+import { GameProvider } from '../contexts/GameContext'
+import { HashRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <GameProvider>
+        <App />
+      </GameProvider>
+    </HashRouter>
   </React.StrictMode>,
 )
